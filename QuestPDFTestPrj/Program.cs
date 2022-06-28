e5a2a915f635a50a41a1df317f45549df52d79aa
snailpjw <alanwei1202@gmail.com>
2022-06-28 09:41:23 +0800

    add example code.

--- QuestPDFTestPrj/Program.cs
+++ QuestPDFTestPrj/Program.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using QuestPDF.Fluent;
+using System.IO;
 
 namespace QuestPDFTestPrj
 {
@@ -10,6 +7,21 @@ namespace QuestPDFTestPrj
     {
         static void Main(string[] args)
         {
+            var model = InvoiceDocumentDataSource.GetInvoiceDetails();
+            var document = new InvoiceDocument(model);
+
+            // Generate PDF file and show it in the default viewer
+            //GenerateDocumentAndShow(document);
+            GenerateDocumentAndShow2(document);
+        }
+        static void GenerateDocumentAndShow2(InvoiceDocument document)
+        {
+            const string filePath = "invoice.pdf";
+
+            byte[] pdfByte = document.GeneratePdf();
+
+            File.WriteAllBytes(filePath, pdfByte);
+
         }
     }
 }
